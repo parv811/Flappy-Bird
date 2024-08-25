793d35a5a22ab0084af2430415300990f13ecb6d
Parv Shah <parvshah2004@gmail.com>
2024-08-26 02:07:24 +0530

    Added some additional features
    1. Added Close button
    2. Added High-Score and Reset High-Score Button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,18 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private Movement movement;
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text highScoreText;
     [SerializeField] private GameObject start;
+    [SerializeField] private GameObject reset;
+    [SerializeField] private GameObject exit;
     [SerializeField] private GameObject GameOverUI;
     private int score;
 
     private void Awake()
     {
         Application.targetFrameRate = 60;
+
+        highScoreText.text = "High-Score\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
 
         Pause();
     }
@@ -23,9 +28,12 @@ public class GameManager : MonoBehaviour
     {        
         score = 0;
         scoreText.text = score.ToString();
+        highScoreText.text = "High-Score\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
 
         start.SetActive(false);
         GameOverUI.SetActive(false);
+        reset.SetActive(false);
+        exit.SetActive(false);
 
         Time.timeScale = 1f;
         movement.enabled = true;    
@@ -43,18 +51,37 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
         movement.enabled = false;
         start.SetActive(true);
+        reset.SetActive(true);
+        exit.SetActive(true);
     }
     public void GameOver()
     {
-        GameOverUI.SetActive(true);
-        start.SetActive(true);
+        GameOverUI.SetActive(true);        
         
         Pause();
+    }
+
+    public void Exit()
+    {        
+        Application.Quit();
     }
 
     public void IncreaseScore()
     {
         score++;
         scoreText.text = score.ToString();
+
+        if(score >= PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = "High-Score\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        }
     }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        highScoreText.text = "High-Score\nXX";
+    }
+
 }
